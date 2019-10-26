6c6c455adb949efe0d7da1cf31eea38fc4ecf90f
Robert <rob.hopland@gmail.com>
2019-10-27 00:46:27 +0200

    New version

---
+++ precache-manifest.867e0a290a42b947975cde16ca4fc819.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/Elm-Set-Theory-Clock/favicon.ico"
+  },
+  {
+    "revision": "7d982090c40d2e3d917f15956bf155c4",
+    "url": "/Elm-Set-Theory-Clock/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/Elm-Set-Theory-Clock/logo.svg"
+  },
+  {
+    "revision": "465a539d5ce9316c16a05f4dccaa5a32",
+    "url": "/Elm-Set-Theory-Clock/manifest.json"
+  },
+  {
+    "revision": "80278fcc3b9aca79a710",
+    "url": "/Elm-Set-Theory-Clock/static/css/main.242caae5.chunk.css"
+  },
+  {
+    "revision": "80278fcc3b9aca79a710",
+    "url": "/Elm-Set-Theory-Clock/static/js/main.80278fcc.chunk.js"
+  },
+  {
+    "revision": "cd26e410a62757859de9",
+    "url": "/Elm-Set-Theory-Clock/static/js/runtime~main.cd26e410.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/Elm-Set-Theory-Clock/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
